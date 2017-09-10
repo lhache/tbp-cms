c7a15bde7e925f5ba71f06e524120ca8c405505a
lhache <hache.louis@gmail.com>
2017-09-10 16:56:07 +0200

    setup local js app

--- themes/custom/app/src/index.js
+++ themes/custom/app/src/index.js
@@ -5,5 +5,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+debugger;
 ReactDOM.render(<App />, document.getElementById('tbp-app'));
 registerServiceWorker();
